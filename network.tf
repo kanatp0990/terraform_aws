c4d5a419340c075376e7fbd49e587c9c1e9c482a
kanatp0990 <keitai@MacBook-Air.local>
2023-06-02 19:54:02 +0900

    first commit

---
+++ network.tf
@@ -0,0 +1,140 @@
+# ---------------------------------------------
+# VPC
+# ---------------------------------------------
+resource "aws_vpc" "vpc" {
+  cidr_block                       = "192.168.0.0/16"
+  instance_tenancy                 = "default"
+  enable_dns_support               = true
+  enable_dns_hostnames             = true
+  assign_generated_ipv6_cidr_block = false
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-vpc"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+# ---------------------------------------------
+# Subnet
+# ---------------------------------------------
+resource "aws_subnet" "public_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id #上のVPCの設定を参照する。参照する情報がresourceの場合はブロックタイプを省略可
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.1.0/24"
+  map_public_ip_on_launch = true #配置するEC2に対してパブリックIPを割り当てる
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-public_subnet_1a"
+    Project = var.project
+    Env     = var.environment
+    Type    = "public"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id #上のVPCの設定を参照する。参照する情報がresourceの場合はブロックタイプを省略可
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.2.0/24"
+  map_public_ip_on_launch = true #配置するEC2に対してパブリックIPを割り当てる
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-public_subnet_1c"
+    Project = var.project
+    Env     = var.environment
+    Type    = "public"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id #上のVPCの設定を参照する。参照する情報がresourceの場合はブロックタイプを省略可
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.3.0/24"
+  map_public_ip_on_launch = false #配置するEC2に対してパブリックIPを割り当てる
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-private_subnet_1a"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id #上のVPCの設定を参照する。参照する情報がresourceの場合はブロックタイプを省略可
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.4.0/24"
+  map_public_ip_on_launch = false #配置するEC2に対してパブリックIPを割り当てる
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-private_subnet_1c"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
+
+# ---------------------------------------------
+# Route Table
+# ---------------------------------------------
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-public-rt"
+    Project = var.project
+    Env     = var.environment
+    Type    = "public"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_1a" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet_1a.id
+}
+
+resource "aws_route_table_association" "public_rt_1c" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet_1c.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-private-rt"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_1a" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1a.id
+}
+
+
+resource "aws_route_table_association" "private_rt_1c" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1c.id
+}
+
+# ---------------------------------------------
+# Internet Gateway
+# ---------------------------------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-igw"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_route" "public_rt_igw" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
